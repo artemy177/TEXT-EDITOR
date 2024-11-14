637551a808c200ede4d60e66a1d58b8fd31633eb
unknown <artemZOV@google.ru>
2024-11-14 09:47:17 +0300

    second

--- sreda.py
+++ sreda.py
@@ -61,8 +61,6 @@ def find():
 #создание окна
 lexer = PythonLexer()
 root = Tk()
-text1 = tk.Text(root)
-text1.pack()
 
 
 
@@ -78,6 +76,7 @@ file_name.pack()
 # добавление метки в поле поиска
 labl1 =  tk.Label (root, text = 'поиск:' )
 labl1.pack()
+
 
 
 # добавление поля текста в одну строку
@@ -100,7 +99,8 @@ text = Text(root)
 text.insert( '1.0' , '''Type your text here''' )
 text.pack(side = BOTTOM)
 from tkinter import *
-
+text1 = tk.Text(root)
+text1.pack()
 # создание окна
 
 button2 = tk.Button(root, text="сохранить", bg ="black",fg="white", command=save_as_file)
@@ -114,10 +114,48 @@ button1.place(x=500, y=570)
 
 
 
+# Создаём теги с разными свойствами, которые будем присваивать соответствующим типам токенов
+text.tag_config("keyword", foreground='blue')
+text.tag_config("string_literal", foreground='red')
+
+# Прописываем соответствие типа токена тегу подсветки
+token_type_to_tag = {
+    Token.Keyword: "keyword",
+    Token.Operator.Word: "keyword",
+    Token.Name.Builtin: "keyword",
+    Token.Literal.String.Single: "string_literal",
+    Token.Literal.String.Double: "string_literal",
+}
 
 
+def get_text_coord(s: str, i: int):
+    """
+    Из индекса символа получить "координату" в виде "номер_строки_текста.номер_символа_в_строке"
+    """
+    for row_number, line in enumerate(s.splitlines(keepends=True), 1):
+        if i < len(line):
+            return f'{row_number}.{i}'
+        
+        i -= len(line)
 
 
+def on_edit(event):
+    # Удалить все имеющиеся теги из текста
+    for tag in text.tag_names():
+        text.tag_remove(tag, 1.0, tk.END)
+    
+    # Разобрать текст на токены
+    s = text1.get(1.0, tk.END)
+    tokens = lexer.get_tokens_unprocessed(s)
+    
+    for i, token_type, token in tokens:
+        print(i, token_type, repr(token))  # Отладочный вывод - тут видно какие типы токенов выдаются
+        j = i + len(token)
+        if token_type in token_type_to_tag:
+            text1.tag_add(token_type_to_tag[token_type], get_text_coord(s, i), get_text_coord(s, j))
+
+    # Сбросить флаг редактирования текста
+    text1.edit_modified(0)
 
 
 text_info = Text (root, yscrollcommand=Scrollbar.set)
